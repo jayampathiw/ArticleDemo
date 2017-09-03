36d6e47208f1a702bdcd2bcf2d3ec413e89ee829
Jayampathi Wijesena <jayampathi@ebuilder.local>
2017-09-03 13:09:52 +0530

    2nd update

--- js/main.js
+++ js/main.js
@@ -1,51 +1,44 @@
-var t = '';
-function gText(e) {
-    pageX = e.pageX;
-    pageY = e.pageY;
-    t = (document.all) ? document.selection.createRange().text : document
-        .getSelection();
+(function () {
+    "use strict";
 
-    // document.getElementById('input').value = t;
-    // document.getElementById('id01').style.display = 'block'
-    var appBanners = document.getElementsByClassName('tools');
-    if(t != '') {
-         console.log(t.toString());
-        
-         for (i = 0; i < appBanners.length; i += 1) {
-            appBanners[i].style.position = "absolute";
-            appBanners[i].style.left = pageX + 5 + 'px';;
-            appBanners[i].style.top = pageY - 55 + 'px';;
-            appBanners[i].style.display = 'block';
-         }
-    } else {
-        for (i = 0; i < appBanners.length; i += 1) {
-            appBanners[i].style.display = 'none';
-         }
+    var showTwitterIcon = function (e) {
+        var text = '';
+        var pageX = e.pageX;
+        var pageY = e.pageY;
+        // text = (document.all) ? document.selection.createRange().text : document
+        //     .getSelection();
+        if (window.getSelection) {
+            text = window.getSelection();
+        } else if (document.getSelection) {
+            text = document.getSelection();
+        } else if (document.selection) {
+            text = document.selection.createRange().text;
+        }
+
+        var tweetObj = document.getElementsByClassName('tools');
+        if (text != '') {
+            console.log(text.toString());
+
+            for (var i = 0; i < tweetObj.length; i += 1) {
+                tweetObj[i].style.position = "absolute";
+                tweetObj[i].style.left = pageX + 5 + 'px';;
+                tweetObj[i].style.top = pageY - 55 + 'px';;
+                tweetObj[i].style.display = 'block';
+                var tweetObj = document.getElementById("tweet");
+                tweetObj.href = "https://twitter.com/share?url=https://codepen.io/jawi1&amp;text="+text.toString();
+            }
+        } else {
+            for (var i = 0; i < tweetObj.length; i += 1) {
+                tweetObj[i].style.display = 'none';
+            }
+        }
     }
 
-   
-
-    // document.querySelector('ul.tools').css({
-    //             'left': pageX + 5,
-    //             'top' : pageY - 55
-    //         }).fadeIn(200);
-
-    //     var el = document.getElementsByClassNameyId('#content');
-
-    // el.style.left = pageX + 5;
-    // el.style.top = pageY - 55;
-
-    // el.style.dispaly = 'block';
-
-    
-
-}
+    document.onmouseup = showTwitterIcon;
+    if (!document.all) {
+        document.captureEvents(Event.MOUSEUP);
+    }
+})();
 
 
-var pageX;
-var pageY;
 
-document.onmouseup = gText;
-if (!document.all) {
-    document.captureEvents(Event.MOUSEUP);
-}
